f0f9f4fb974c636191988786391af962699f96c6
Desmond-Fon <desmondfon@yahoo.com>
2024-12-11 17:41:09 +0100

    edits

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -10,11 +10,19 @@ const Landing = () => {
             <div className="flex justify-between items-center pt-[3%] px-[3%]">
                 <button className="border-[2px] border-black rounded-[5px] py-[6px] lg:py-[10px] px-[20px] lg:px-[50px] font-sigmar text-black bg-secondary transform transition-transform duration-300 ease-in-out scale-100 hover:scale-110">BUY NOW</button>
 
-                <div className='flex justify-end items-center gap-4'>
-                    <img src={x} alt="" /><img src={dex} alt="" /><img src={tg} alt="" />
+                <div className='flex justify-end items-center gap-2 lg:gap-4'>
+                    <img src={x} alt="" className='w-[30px] lg:w-auto' /><img src={dex} alt="" className='w-[30px] lg:w-auto' /><img src={tg} alt="" className='w-[30px] lg:w-auto' />
                 </div>
             </div>
-            <div className='absolute right-5 bottom-[6%] flex justify-center flex-col items-center'>
+
+            <div className='flex justify-center items-center mt-[20vh] flex-col lg:hidden'>
+                <img src={text} alt="" className='' />
+                <div className='font-sigmar flex justify-start gap-3 bg-white w-[95%] border-[2px] border-black rounded-[8px] overflow-hidden'>
+                    <p className='bg-yellow border-r-[2px] border-r-black px-3 py-2 text-[13px]'>BUY</p>
+                    <p className='py-2 text-[13px]'>CA : 5RSOXXTWFBSSGJWVWVSY</p>
+                </div>
+            </div>
+            <div className='absolute right-5 bottom-[6%] hidden lg:flex justify-center flex-col items-center'>
                 <img src={text} alt="" className='w-[50vw]' />
                 <div className='font-sigmar flex justify-start -mt-[60px] gap-3 bg-white w-[60%] border-[2px] border-black rounded-[8px] overflow-hidden'>
                     <p className='bg-yellow border-r-[2px] border-r-black px-3 py-2'>BUY</p>
