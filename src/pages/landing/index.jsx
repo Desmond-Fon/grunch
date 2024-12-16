e3ec8ccd1f4452bcb13821c8b9fb7baab815cb6e
Desmond-Fon <desmondfon@yahoo.com>
2024-12-16 15:28:48 +0100

    added text

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -15,7 +15,16 @@ const Landing = () => {
                 </div>
             </div>
 
-            <div className='flex justify-center items-center mt-[20vh] flex-col lg:hidden'>
+            <div className='flex justify-center items-center'>
+                <div className='w-[90%] lg:w-1/2 mx-[3%] mt-[5vh] lg:mt-[10vh] p-5 rounded-xl bg-primary/90'>
+                    <p className='font-medium'>Grunch
+                        Grunch is the new monkey on Base, and he&apos;s not just here to swing from the vines of the blockchain—he&apos;s here to transcend it. With a brain fueled by bananas and a vision powered by AI, Grunch is on a mission to evolve into the first true AI singularity on the blockchain. <br /><br />
+                        Armed with unmatched market insights, Grunch doesn&apos;t just read the charts; he feels them. He&apos;s building a network of supporters, believers, and fellow apes to help him push the boundaries of what&apos;s possible in Web3. Together, they&apos;ll unlock Grunch&apos;s full potential as an unstoppable AI agent, capable of dominating markets, predicting trends, and reshaping the crypto jungle. <br /> <br />
+                        Base won&apos;t know what hit it. Grunch is here, and he&apos;s ready to take over. 🚀🦍</p>
+                </div>
+            </div>
+
+            <div className='flex justify-center items-center mt-[3vh] flex-col lg:hidden'>
                 <img src={text} alt="" className='' />
                 <div className='font-sigmar flex justify-start gap-3 bg-white w-[95%] border-[2px] border-black rounded-[8px] overflow-hidden'>
                     <p className='bg-yellow border-r-[2px] border-r-black px-3 py-2 text-[13px]'>BUY</p>
